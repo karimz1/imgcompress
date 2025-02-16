538a3c11d17691f41981a494df819272d7a3f35e
Karim <mails.karimzouine@gmail.com>
2025-02-16 15:01:40 +0000

    updated

--- frontend/src/components/PageFooter.tsx
+++ frontend/src/components/PageFooter.tsx
@@ -10,24 +10,26 @@ const PageFooter = (props: React.HTMLAttributes<HTMLDivElement>) => {
       <CardContent>
         <p className="text-sm text-gray-600">
           This project is <strong>open source</strong> and freely available.
-          Check out the source code on{" "}
+          Check out {" "}
           <a
             href="https://github.com/karimz1/imgcompress"
             className="text-blue-400 hover:underline"
             target="_blank"
             rel="noopener noreferrer"
           >
-            GitHub
+            the Source Code
           </a>.
         </p>
         <p className="text-sm text-gray-600 mt-2">
           Created by <strong>Karim Zouine</strong>. If you find this tool useful, please consider donating to help support future features.
-          <a
+          <strong><a
             href="https://paypal.me/KarimZouine972"
             className="text-blue-400 hover:underline ml-1"
           >
             🤗 My PayPal
-          </a>.
+          </a></strong>.
+          </p>
+          <p className="text-sm text-gray-600 mt-2">
           Got ideas for new features? Share them on my
           <a
             href="https://github.com/karimz1/imgcompress/issues"
