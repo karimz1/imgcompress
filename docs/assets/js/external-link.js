07758459328b9ae77d9709fc5b843331e7ac1f77
Karim Z. <mails.karimzouine@gmail.com>
2026-04-06 16:15:01 +0200

    chore: remove legacy docs site and infra scaffolding
    Removes the old MkDocs-based documentation assets and accompanying AWS CDK infrastructure scaffolding. This cleanup deletes legacy docs files, styling, and infra configuration to simplify maintenance and prepare for a refreshed docs workflow.

--- docs/assets/js/external-link.js
+++
@@ -1,9 +0,0 @@
-// External links handler - security best practices
-(function () {
-    'use strict';
-    var links = document.querySelectorAll('a[href^="http"]:not([href*="' + location.hostname + '"])');
-    for (var i = 0; i < links.length; i++) {
-        links[i].target = '_blank';
-        links[i].rel = 'noopener noreferrer';
-    }
-})();
